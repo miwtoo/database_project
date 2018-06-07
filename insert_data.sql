ad69e3239a949edac2b35432389c19265bf75d69
DESKTOP-5LSDE5P\Miwtoo-NB <miwyugioh@gmail.com>
2018-06-07 17:14:31 +0700

    Change

---
+++ insert_data.sql
@@ -0,0 +1,22 @@
+INSERT INTO writer VALUES(1 , ‘อิสญะ ตระกูลพุทธรักษา’ , null , null);
+
+INSERT INTO writer VALUES(2 , ‘จุฑาธิป พึ่งละออ’ , null , null);
+
+INSERT INTO writer VALUES(3 , ‘Pensri Kiengsiri’ , null , null);
+
+INSERT INTO writer VALUES(4 , ‘Ha-Hyun’ , ‘Hyunmeen’ , null);
+
+INSERT INTO writer VALUES(5 , ‘นิตยสารเกมออนไลน์’ , null , null);
+
+
+
+
+INSERT INTO book VALUES(1,'นาซ่าก็พาเธอกลับมาไม่ได้', 'ทั่วไป', 185.25, 9786161823269 );
+
+INSERT INTO book VALUES(2, 'New English Grammar & Vocabulary Book 2', 'การศึกษา', 160.55,  9789744329707);
+
+INSERT INTO book VALUES(3, 'The Sky So Near', 'วรรณกรรม', 560.50,  9786165101325 );
+
+INSERT INTO book VALUES(4, 'A Piece of the Moon', 'วรรณกรรม', 284.05, 9786160621989);
+
+INSERT INTO book VALUES(5, 'OS Online Station No.683', 'นิตยสาร', 28.05, 9772032683001);
